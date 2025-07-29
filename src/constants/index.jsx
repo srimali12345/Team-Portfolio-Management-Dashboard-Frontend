b4123e2d909bc73d5604677e1906765f7cc868eb
srimali <srimali.ranawaka@1billiontech.com>
2025-07-29 14:57:42 +0530

    file changes

--- src/constants/index.jsx
+++ src/constants/index.jsx
@@ -1,26 +1,22 @@
 export const FORM_CONSTANTS = {
-
-    COMMON:{
-        ERROR_MESSAGE: "An error occurred. Please try again.",
-         USERNAME_LABEL: "Username",
-        PASSWORD_LABEL: "Password",
-        EMAIL_LABEL: "Email",
-        ERROR_MESSAGE: "All fields are required.",
-        ERROR_MESSAGE_USERNAME: "Username is required.",
-        ERROR_MESSAGE_PASSWORD: "Password is required.",
-        ERROR_MESSAGE_EMAIL: "Email is required.",
-        CONFIRM_PASSWORD_LABEL: "Confirm Password",
-    },
-    LOGIN: {
-        TITLE: "Login",
-        SUBTITLE: "Welcome back! Please login to your account.",
-        LOGIN_LINK_TEXT: "Already have an account? Login now.",
-    },
-    REGISTER:{
-        TITLE: "Register",
-        SUBTITLE: "Create a new account to get started.",
-        ERROR_MESSAGE: "All fields are required.",
-        CONFIRM_PASSWORD_LABEL: "Confirm Password",
-    }
-
-}
+  COMMON: {
+    USERNAME_LABEL: "Username",
+    PASSWORD_LABEL: "Password",
+    EMAIL_LABEL: "Email",
+    ERROR_MESSAGE: "All fields are required.",
+    ERROR_MESSAGE_USERNAME: "Username is required.",
+    ERROR_MESSAGE_PASSWORD: "Password is required.",
+    ERROR_MESSAGE_EMAIL: "Email is required.",
+    CONFIRM_PASSWORD_LABEL: "Confirm Password",
+  },
+  LOGIN: {
+    TITLE: "Login",
+    SUBTITLE: "Welcome back! Please login to your account.",
+    LOGIN_LINK_TEXT: "Already have an account? Login now.",
+  },
+  REGISTER: {
+    TITLE: "Register",
+    SUBTITLE: "Create a new account to get started.",
+    CONFIRM_PASSWORD_LABEL: "Confirm Password",
+  },
+};
